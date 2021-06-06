e6d5c86037723ad10d79db856f1df00404df51d6
Co9xs <ryo.fjsm0203@icloud.com>
2021-06-06 17:10:35 +0900

    implement loopup type

---
+++ src/medium/TypeLookup.ts
@@ -0,0 +1,16 @@
+type LookUp<U extends {type: string}, T> = U extends {type: T} ? U : never
+
+declare const dog: LookUp<Animal, 'dog'>
+
+interface Cat {
+  type: 'cat'
+  breeds: 'Abyssinian' | 'Shorthair' | 'Curl' | 'Bengal'
+}
+
+interface Dog {
+  type: 'dog'
+  breeds: 'Hound' | 'Brittany' | 'Bulldog' | 'Boxer'
+  color: 'brown' | 'white' | 'black'
+}
+
+type Animal = Cat | Dog
