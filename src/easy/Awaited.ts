9f7d5c134f4c2497acda538cdc2326c6f0557fcc
Co9xs <ryo.fjsm0203@icloud.com>
2021-06-14 08:32:52 +0900

    fix awaited

--- src/easy/Awaited.ts
+++ src/easy/Awaited.ts
@@ -5,3 +5,9 @@ type Awaited<T extends Promise<any>> = T extends Promise<infer R> ? R : never
 
 let resultX: Awaited<X>
 let resultY: Awaited<Y>
+
+type Awaited2<T extends Promise<unknown>> = T extends Promise<infer R> ? R : never
+
+
+let resultX2: Awaited2<X>
+let resultY2: Awaited2<Y>
